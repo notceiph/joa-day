3ec8f1649c50ca058ba430d7b0a7d01248a92175
Eli <105756319+Eli-jaah@users.noreply.github.com>
2024-10-21 19:13:10 -0400

    Fixed the padding under the crossword puzzle, still need to fix the clue box size

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Interactive Crossword Puzzle</h1>
+      <h1>wtf its ur birthday joa</h1>
+      <h4>this is part 1 pal i hope you have fun</h4>
       <Crossword />
     </div>
   );
